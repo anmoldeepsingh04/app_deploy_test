76d43395ebe0be6a16c237d76884a41539950248
Anmoldeep Singh <go93net@mytum.de>
2026-06-22 15:37:39 +0200

    Defined __init__ file with OOP package

--- steel_microstructure/simulator/__init__.py
+++ steel_microstructure/simulator/__init__.py
@@ -1,1 +1,17 @@
+from .constants import COLORS
 from .factory import create_unified_simulator
+from .base import BaseSimulator
+from .iron import IronCarbonPhaseDiagramSimulator
+from .steel import SteelMicrostructureSimulator
+from .cast_iron import GeneralizedSteel
+from . import transitions
+
+__all__ = [
+    "COLORS",
+    "create_unified_simulator",
+    "BaseSimulator",
+    "IronCarbonPhaseDiagramSimulator",
+    "SteelMicrostructureSimulator",
+    "GeneralizedSteel",
+    "transitions"
+]
